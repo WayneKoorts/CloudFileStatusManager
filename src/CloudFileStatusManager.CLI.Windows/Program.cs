c2e714efb41dd33bce4f8d56585879f9093eb805
Wayne Koorts <wayne@wkoorts.com>
2025-03-09 13:40:23 +1300

    Start building out Windows CLI.

--- src/CloudFileStatusManager.CLI.Windows/Program.cs
+++ src/CloudFileStatusManager.CLI.Windows/Program.cs
@@ -1,4 +1,23 @@
-﻿using CloudFileStatusManager;
+﻿using System.CommandLine;
+using CloudFileStatusManager;
 using CloudFileStatusManager.Windows;
 
 ICloudFileStatusManager cloudFileStatusManager = new WindowsCloudFileStatusManager();
+
+var rootCommand = new RootCommand("Cloud File Status Manager CLI for Windows");
+
+var fileArgument = new Argument<string>("file", "The path of the file to manage");
+
+var getHydrationStatusCommand = new Command("get-hydration-status", "Get the hydration status of a file")
+{
+    fileArgument
+};
+getHydrationStatusCommand.SetHandler(filePath =>
+{
+    var status = cloudFileStatusManager.GetHydrationStatus(filePath);
+    Console.WriteLine(status);
+}, fileArgument);
+
+rootCommand.AddCommand(getHydrationStatusCommand);
+
+await rootCommand.InvokeAsync(args);
