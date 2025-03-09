d7ee3ab803d977e53a3a5fb19f7ea9a8d5ba3fb9
Wayne Koorts <wayne@wkoorts.com>
2025-03-09 22:58:19 +1300

    Add functions for setting file attributes.

--- src/CloudFileStatusManager.Windows/ExternalFileUtils.cs
+++ src/CloudFileStatusManager.Windows/ExternalFileUtils.cs
@@ -9,8 +9,10 @@ public static class ExternalFileUtils
     // Windows API constants for cloud file attributes.
     internal const int FILE_ATTRIBUTE_PINNED = 0x00080000;
     internal const int FILE_ATTRIBUTE_UNPINNED = 0x00100000;
+    internal const int FILE_ATTRIBUTE_OFFLINE = 0x00001000;
     internal const int FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS = 0x00400000;
     private const int FILE_READ_ATTRIBUTES = 0x0080;
+    private const int FILE_WRITE_ATTRIBUTES = 0x0100;
     private const int FILE_SHARE_READ = 0x00000001;
     private const int FILE_SHARE_WRITE = 0x00000002;
     private const int OPEN_EXISTING = 3;
@@ -32,6 +34,14 @@ public static class ExternalFileUtils
         IntPtr hFile,
         FILE_INFO_BY_HANDLE_CLASS FileInformationClass,
         out FILE_BASIC_INFO lpFileInformation,
+        uint dwBufferSize
+    );
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool SetFileInformationByHandle(
+        IntPtr hFile,
+        FILE_INFO_BY_HANDLE_CLASS FileInformationClass,
+        ref FILE_BASIC_INFO lpFileInformation,
         uint dwBufferSize
     );
 
@@ -66,6 +76,67 @@ public static class ExternalFileUtils
 
         if (hFile.ToInt64() == -1) throw new IOException("Failed to open file.");
 
+        FILE_BASIC_INFO info;
+        try
+        {
+            if (!GetFileInformationByHandleEx(
+                    hFile,
+                    FILE_INFO_BY_HANDLE_CLASS.FileBasicInfo,
+                    out info,
+                    (uint)Marshal.SizeOf<FILE_BASIC_INFO>()))
+            {
+                throw new IOException("GetFileInformationByHandleEx call failed.");
+            }
+        }
+        finally
+        {
+            CloseHandle(hFile);
+        }
+
+        return info.FileAttributes;
+    }
+
+    /// <summary>
+    /// Sets the specified attributes on the file at the specified path.
+    /// </summary>
+    /// <param name="filePath"></param>
+    /// <param name="setAttributes">List of attributes to set.</param>
+    /// <param name="unsetAttributes">List of attributes to unset.</param>
+    /// <exception cref="FileNotFoundException"></exception>
+    /// <exception cref="IOException">
+    /// Thrown if there is a problem updating the attributes
+    /// on the file.
+    /// </exception>
+    /// <seealso cref="FILE_ATTRIBUTE_PINNED" />
+    /// <seealso cref="FILE_ATTRIBUTE_UNPINNED" />
+    /// <seealso cref="FILE_ATTRIBUTE_OFFLINE" />
+    /// <seealso cref="FILE_ATTRIBUTE_RECALL_ON_DATA_ACCESS" />
+    internal static void SetAttributes(
+        string filePath,
+        uint[]? setAttributes = null,
+        uint[]? unsetAttributes = null)
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException("File not found", filePath);
+        }
+
+        if (setAttributes is { Length: 0 } && unsetAttributes is { Length: 0 })
+        {
+            return;
+        }
+
+        var hFile = CreateFile(
+            filePath,
+            FILE_WRITE_ATTRIBUTES,
+            FILE_SHARE_READ | FILE_SHARE_WRITE,
+            IntPtr.Zero,
+            OPEN_EXISTING,
+            0,
+            IntPtr.Zero);
+
+        if (hFile.ToInt64() == -1) throw new IOException("Failed to open file.");
+
         try
         {
             if (!GetFileInformationByHandleEx(
@@ -76,7 +147,31 @@ public static class ExternalFileUtils
             {
                 throw new IOException("GetFileInformationByHandleEx call failed.");
             }
-            return info.FileAttributes;
+
+            if (setAttributes is { Length: > 0 })
+            {
+                foreach (var attribute in setAttributes)
+                {
+                    info.FileAttributes |= attribute;
+                }
+            }
+
+            if (unsetAttributes is { Length: > 0 })
+            {
+                foreach (var attribute in unsetAttributes)
+                {
+                    info.FileAttributes &= ~attribute;
+                }
+            }
+
+            if (!SetFileInformationByHandle(
+                    hFile,
+                    FILE_INFO_BY_HANDLE_CLASS.FileBasicInfo,
+                    ref info,
+                    (uint)Marshal.SizeOf<FILE_BASIC_INFO>()))
+            {
+                throw new IOException("SetFileInformationByHandle call failed.");
+            }
         }
         finally
         {
